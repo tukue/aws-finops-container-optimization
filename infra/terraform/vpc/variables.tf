dd2f2c2be01aab9e6bb5c70ebb57b1bae5ac962a
Tukue Gebregergis <tukue.geb@gmail.com>
2026-02-03 20:40:07 +0100

    Update Terraform testing to use eu-west-1 region

--- infra/terraform/vpc/variables.tf
+++ infra/terraform/vpc/variables.tf
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
 }
 
 variable "common_tags" {
